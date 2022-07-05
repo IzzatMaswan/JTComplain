8c50ff58d407fc75879c9100d5d5403d332b2fa7
IzzatMaswan <103076901+IzzatMaswan@users.noreply.github.com>
2022-07-05 17:31:45 +0800

    c10

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -3,7 +3,7 @@
             <li><a href="profile.php">Profile Settings</a></li>
               <li><a href="update-password.php">Update Password</a></li>
             <li><a href="post-complaint.php">Post a complaint</a></li>
-               <li><a href="my-complaints.php">My complaints</a></li>
+               <li><a href="my-complaint.php">My complaints</a></li>
             <li><a href="logout.php">Sign Out</a></li>
           </ul>
         </div>
