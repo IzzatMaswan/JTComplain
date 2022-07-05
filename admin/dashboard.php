b5e34a29a4ada0635beb16be02f198f1bf3f0425
IzzatMaswan <103076901+IzzatMaswan@users.noreply.github.com>
2022-07-05 19:19:52 +0800

    c18

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -82,9 +82,9 @@ $sql3 ="SELECT id from tbldepartment ";
 $query3= $dbh -> prepare($sql3);
 $query3->execute();
 $results3=$query3->fetchAll(PDO::FETCH_OBJ);
-$brands=$query3->rowCount();
+$department=$query3->rowCount();
 ?>												
-													<div class="stat-panel-number h1 "><?php echo htmlentities($brands);?></div>
+													<div class="stat-panel-number h1 "><?php echo htmlentities($department);?></div>
 													<div class="stat-panel-title text-uppercase">Listed Department</div>
 												</div>
 											</div>
